234fb2717ae11c9610ad88d8370e1914870fc465
UdayBari3421 <udaybari2003@gmail.com>
2024-02-13 19:44:58 +0530

    1.0

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,11 @@
+import React from "react";
 
+const Selection = (props) => {
+  return (
+    <div className="fix-box" onClick={(e) => props.applyColor(e.target)}>
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+};
+
+export default Selection;
