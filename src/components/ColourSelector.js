071ff9400094725284f876f8ae5510d98320d467
UdayBari3421 <udaybari2003@gmail.com>
2024-02-13 19:50:16 +0530

    2.0

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -5,6 +5,7 @@ const ColourSelector = (props) => {
   const { background } = config;
   return (
     <button
+      data-test-id={config.key}
       className={config.classname}
       onClick={() => selectNextBackground({ background })}
     >
